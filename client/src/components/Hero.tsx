1d4ff43dcd1f53c46cbb1f1e135d4497a87923c6
haitran-rikai <hai.tran@rikai.technology>
2021-10-03 21:30:12 +0700

    check function component

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,17 +1,25 @@
-import { Flex, Heading } from '@chakra-ui/react'
+import { Flex, Heading } from '@chakra-ui/react';
 
-export const Hero = ({ title }: { title: string }) => (
-  <Flex
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-l, #7928CA, #FF0080)"
-    bgClip="text"
-  >
-    <Heading fontSize="6vw">{title}</Heading>
-  </Flex>
-)
+type PropsType = {
+  title: string;
+};
+
+export const Hero = (props: PropsType) => {
+  const { title } = props;
+
+  return (
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      bgGradient="linear(to-l, #7928CA, #FF0080)"
+      bgClip="text"
+    >
+      <Heading fontSize="6vw">{title}</Heading>
+    </Flex>
+  );
+};
 
 Hero.defaultProps = {
   title: 'with-chakra-ui-typescript',
-}
+};
